a5fc0cfadf8491ba57408f38bc4ab663f5013be1
chenjiahe <MTDTonu@126.com>
2020-06-05 16:19:12 +0800

    1

--- demo/src/com/itcast/demo/test.java
+++ demo/src/com/itcast/demo/test.java
@@ -3,5 +3,7 @@ package com.itcast.demo;
 public class test {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("xxx");
+        System.out.println("xxx");
     }
 }
